0f20f61ec6ad014059543104ac68d3b9c127c3cf
Jorge Avila <jorgeeavila1@gmail.com>
2021-04-07 18:28:49 -0400

    changed to pyautogui

--- Code/code.py
+++ Code/code.py
@@ -1,13 +1,10 @@
-import datetime as dt
-import subprocess
+import os
+import pyautogui
+import time
+from time import sleep
+from datetime import datetime
 
-current_datetime = dt.datetime.now()
-print(current_datetime)
 
-string_date = current_datetime.strftime("%b %d, %I:%M:%S%p")
-print(string_date)
 
-if string_date == 'Apr 06, 12:09:00PM':
-    print('yes!')
-else:
-    subprocess.call(['C:\Users\jorge\Desktop\Prive Stuff\helebn.helebn.png'])
+date = datetime.now().strftime("%b %d")
+print(date)
