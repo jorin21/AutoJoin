ce953a2b27d14efa889154da70dd77d7ce3e12f9
Jorge Avila <jorgeeavila1@gmail.com>
2021-04-07 20:59:36 -0400

    added split and time

--- Code/code.py
+++ Code/code.py
@@ -1,10 +1,25 @@
 import os
-import pyautogui
 import time
+import pyautogui
 from time import sleep
 from datetime import datetime
 
 
-
+x = 1
+print("starting")
 date = datetime.now().strftime("%b %d")
-print(date)
+time = datetime.now().strftime("%I:%M%p")
+with open('Dates.txt', 'r') as read_obj:
+    for line in read_obj:
+        if date in line:
+            ss = line.split("=")
+            if ss[1].strip() == 'b':
+                print(time)
+                if time == "08:44PM":
+                    print(time)
+                    print(x)
+                    x = x + 1
+                    print("voy")
+                    os.startfile("C:/Users/jorge/Desktop/Auto MS Teams Code/AutoJoin/Code/Classes/Class 1.lnk") 
+
+    
