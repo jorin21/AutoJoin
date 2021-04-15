77864397840502a61ad0d4b0a73c7736227b1753
Jorge Avila <jorgeeavila1@gmail.com>
2021-04-15 11:09:02 -0400

    Added new branch for the laptop code

--- Code/main.py
+++ Code/main.py
@@ -10,44 +10,143 @@ date = datetime.now().strftime("%b %d")
 time = datetime.now().strftime("%I:%M%p")
 
 
-with open("C:/Users\jorge\Desktop\Python AutoJoin Script\Code\Dates.txt", 'r') as read_obj:
+with open("C:/Users\jorge\Desktop\Auto MS Teams Code\AutoJoin\Code\Dates.txt", 'r') as read_obj:
     for line in read_obj:
         if date in line:
             ss = line.split("=")
-            if ss[1].strip() == 'b':
-                if time == "07:40AM":
+            if ss[1].strip() == 's':
+                if time >= "07:35AM" and time < "09:10AM":
                     print(time)
-                    os.startfile("C:/Users\jorge\Desktop\Python AutoJoin Script\Code\Classes\Class 1.lnk")
+                    os.startfile("C:/Users\jorge\Desktop\Auto MS Teams Code\AutoJoin\Code\Classes\Class 1.lnk")
                     sleep(1)
                     print('.')
-                    sleep(9)
-                    mute = pyautogui.locateCenterOnScreen("C:/Users\jorge\Desktop\Python AutoJoin Script\Code\Images\mute.png") 
+                    sleep(24)
+                    mute = pyautogui.locateCenterOnScreen("C:/Users\jorge\Desktop\Auto MS Teams Code\AutoJoin\Code\Images\mute.png") 
                     pyautogui.moveTo(mute)
                     pyautogui.click()
                     sleep(1)
 
-                    join = pyautogui.locateCenterOnScreen("C:/Users\jorge\Desktop\Python AutoJoin Script\Code\Images\join.png") 
+                    join = pyautogui.locateCenterOnScreen("C:/Users\jorge\Desktop\Auto MS Teams Code\AutoJoin\Code\Images\join.png") 
                     pyautogui.moveTo(join)
                     pyautogui.click()
                     sleep(2)
-                    print('Enjoy Your Class!')
-            if ss[1].strip() == 's':
-                if time == "07:40AM":
+                    print('Enjoy Physics Jorge!')
+                if time >= "09:15AM" and time < "10:48AM":
                     print(time)
-                    os.startfile("C:/Users/jorge/Desktop/Python AutoJoin Script/Code/Classes/Class 2.lnk")
+                    os.startfile("C:/Users\jorge\Desktop\Auto MS Teams Code\AutoJoin\Code\Classes\Class 2.lnk")
+                    sleep(1)
+                    print('.')
+                    sleep(24)
+                    mute = pyautogui.locateCenterOnScreen("C:/Users\jorge\Desktop\Auto MS Teams Code\AutoJoin\Code\Images\mute.png") 
+                    pyautogui.moveTo(mute)
+                    pyautogui.click()
+                    sleep(1)
+
+                    join = pyautogui.locateCenterOnScreen("C:/Users\jorge\Desktop\Auto MS Teams Code\AutoJoin\Code\Images\join.png") 
+                    pyautogui.moveTo(join)
+                    pyautogui.click()
+                    sleep(2)
+                    print('Enjoy Engineering Jorge!')
+                if time >= "10:55AM" and time < "11:20am":
+                    print(time)
+                    print("no classes right now")
+                if time == "11:31AM":
+                    print(time)
+                    os.startfile("C:/Users\jorge\Desktop\Auto MS Teams Code\AutoJoin\Code\Classes\Class 3.lnk")
+                    sleep(1)
+                    print('.')
+                    sleep(24)
+                    mute = pyautogui.locateCenterOnScreen("C:/Users\jorge\Desktop\Auto MS Teams Code\AutoJoin\Code\Images\mute.png") 
+                    pyautogui.moveTo(mute)
+                    pyautogui.click()
+                    sleep(1)
+
+                    join = pyautogui.locateCenterOnScreen("C:/Users\jorge\Desktop\Auto MS Teams Code\AutoJoin\Code\Images\join.png") 
+                    pyautogui.moveTo(join)
+                    pyautogui.click()
+                    sleep(2)
+                    print('Enjoy Stats Jorge!')
+                if time == "01:05PM":
+                    print(time)
+                    os.startfile("C:/Users\jorge\Desktop\Auto MS Teams Code\AutoJoin\Code\Classes\Class 4.lnk")
+                    sleep(1)
+                    print('.')
+                    sleep(24)
+                    mute = pyautogui.locateCenterOnScreen("C:/Users\jorge\Desktop\Auto MS Teams Code\AutoJoin\Code\Images\mute.png") 
+                    pyautogui.moveTo(mute)
+                    pyautogui.click()
+                    sleep(1)
+
+                    join = pyautogui.locateCenterOnScreen("C:/Users\jorge\Desktop\Auto MS Teams Code\AutoJoin\Code\Images\join.png") 
+                    pyautogui.moveTo(join)
+                    pyautogui.click()
+                    sleep(2)
+                    print('Enjoy Econ Jorge!')
+            if ss[1].strip() == 'b':
+                if time >= "07:35AM" and time < "09:10AM":
+                    print(time)
+                    os.startfile("C:/Users\jorge\Desktop\Auto MS Teams Code\AutoJoin\Code\Classes/Class 5.lnk")
                     sleep(1)
                     print(".")
-                    sleep(9)
-                    mute = pyautogui.locateCenterOnScreen("Images/mute.PNG") 
+                    sleep(24)
+                    mute = pyautogui.locateCenterOnScreen("C:/Users\jorge\Desktop\Auto MS Teams Code\AutoJoin\Code\Images\mute.png") 
                     pyautogui.moveTo(mute)
                     pyautogui.click()
                     sleep(1)
 
-                    join = pyautogui.locateCenterOnScreen("Images/join.PNG") 
+                    join = pyautogui.locateCenterOnScreen("C:/Users\jorge\Desktop\Auto MS Teams Code\AutoJoin\Code\Images\join.png") 
                     pyautogui.moveTo(join)
                     pyautogui.click()
                     sleep(2)
-                    print('Enjoy Your Class!')
+                    print('Enjoy English Jorge!')
+                if time >= "09:15AM" and time < "10:48AM":
+                    print(time)
+                    os.startfile("C:/Users\jorge\Desktop\Auto MS Teams Code\AutoJoin\Code\Classes/Class 6.lnk")
+                    sleep(1)
+                    print(".")
+                    sleep(24)
+                    mute = pyautogui.locateCenterOnScreen("C:/Users\jorge\Desktop\Auto MS Teams Code\AutoJoin\Code\Images\mute.png") 
+                    pyautogui.moveTo(mute)
+                    pyautogui.click()
+                    sleep(1)
+
+                    join = pyautogui.locateCenterOnScreen("C:/Users\jorge\Desktop\Auto MS Teams Code\AutoJoin\Code\Images\join.png") 
+                    pyautogui.moveTo(join)
+                    pyautogui.click()
+                    sleep(2)
+                    print('Enjoy Study Hall Jorge!')
+                if time == "10:52AM":
+                    print(time)
+                    os.startfile("C:/Users\jorge\Desktop\Auto MS Teams Code\AutoJoin\Code\Classes/Class 7.lnk")
+                    sleep(1)
+                    print(".")
+                    sleep(24)
+                    mute = pyautogui.locateCenterOnScreen("C:/Users\jorge\Desktop\Auto MS Teams Code\AutoJoin\Code\Images\mute.png") 
+                    pyautogui.moveTo(mute)
+                    pyautogui.click()
+                    sleep(1)
+
+                    join = pyautogui.locateCenterOnScreen("C:/Users\jorge\Desktop\Auto MS Teams Code\AutoJoin\Code\Images\join.png") 
+                    pyautogui.moveTo(join)
+                    pyautogui.click()
+                    sleep(2)
+                    print('Enjoy Psych Jorge!')
+                if time == "01:05PM":
+                    print(time)
+                    os.startfile("C:/Users\jorge\Desktop\Auto MS Teams Code\AutoJoin\Code\Classes/Class 8.lnk")
+                    sleep(1)
+                    print(".")
+                    sleep(24)
+                    mute = pyautogui.locateCenterOnScreen("C:/Users\jorge\Desktop\Auto MS Teams Code\AutoJoin\Code\Images\mute.png") 
+                    pyautogui.moveTo(mute)
+                    pyautogui.click()
+                    sleep(1)
+
+                    join = pyautogui.locateCenterOnScreen("C:/Users\jorge\Desktop\Auto MS Teams Code\AutoJoin\Code\Images\join.png") 
+                    pyautogui.moveTo(join)
+                    pyautogui.click()
+                    sleep(2)
+                    print('Enjoy Media Studs Jorge!')
 
 
     
--- Code/run.bat
+++ Code/run.bat
@@ -1,2 +1,2 @@
-"C:\Users\jorge\AppData\Local\Programs\Python\Python39\python.exe" "C:\Users\jorge\Desktop\Python AutoJoin Script\Code\main.py"
+"C:\Users\jorge\AppData\Local\Programs\Python\Python39\python.exe" "C:\Users\jorge\Desktop\Auto MS Teams Code\AutoJoin\Code\main.py"
 pause
